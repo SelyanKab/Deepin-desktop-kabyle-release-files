695262eea9167acd8adf1fd380d605684465cfc9
SelyanKab <selyan.kab@protonmail.com>
2021-05-18 08:52:38 +0200

    add deepin-desktop ts files

---
+++ Deepin-browser/deepin-browser_kab.ts
@@ -0,0 +1,231 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="kab" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../QObjectTS.cpp" line="3"/>
+        <source>Search or type a URL</source>
+        <translation>Nadi neɣ sekcem tansa URL</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="4"/>
+        <source>It is not your default browser</source>
+        <translation>Mačči d iminig-inek·inem amezwer</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="5"/>
+        <source>Search engine used in the address bar</source>
+        <translation>Amsedday n unadi ittwasqedcen deg ufeggag n tensa</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="6"/>
+        <source>Close tabs to the right</source>
+        <translation>Mdel icarren ɣer uyfus</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="7"/>
+        <source>Authentication is required to view passwords</source>
+        <translation>Yesra usesteb akken ad twaliḍ awalen uffiren</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="8"/>
+        <location filename="../resourceTS.cpp" line="4"/>
+        <source>Replace with current URL</source>
+        <translation>Beddel s URL amiran</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="10"/>
+        <source>Images</source>
+        <translation>Tugniwin</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="11"/>
+        <source>News</source>
+        <translation>Isallen</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="12"/>
+        <source>Maps</source>
+        <translation>Tikarḍiwin</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="13"/>
+        <source>Search</source>
+        <translation>Nadi</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="14"/>
+        <source>More</source>
+        <translation>Ugar</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="15"/>
+        <source>Name</source>
+        <translation>Isem</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="16"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="18"/>
+        <source>Incognito</source>
+        <translation>Uslig</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="20"/>
+        <source>Baidu</source>
+        <translation>Baidu</translation>
+    </message>
+    <message>
+        <location filename="../QObjectTS.cpp" line="21"/>
+        <source>Sogou</source>
+        <translation>Sogou</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="3"/>
+        <source>Reload all tabs</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="5"/>
+        <source>Pin this tab</source>
+        <translation>Senṭeḍ iccer-a</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="6"/>
+        <source>Unpin this tab</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="7"/>
+        <source>Close this tab</source>
+        <translation>Kkes iccer-a</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="8"/>
+        <source>Open new tab</source>
+        <translation>Ldi iccer amaynut</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="9"/>
+        <source>No bookmarks yet</source>
+        <translation>Ulac yakan icarren</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="10"/>
+        <source>General</source>
+        <translation>Agejdan</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="11"/>
+        <source>Details</source>
+        <translation>Talqayt</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="12"/>
+        <source>Cancel</source>
+        <translation>Sefsex</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="13"/>
+        <source>Webpage</source>
+        <translation>Asebter Web</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="14"/>
+        <source>Videos</source>
+        <translation>Tividyutin</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="15"/>
+        <source>optional</source>
+        <translation>afrayan</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="17"/>
+        <source>Theme</source>
+        <translation>Asentel</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="18"/>
+        <source>Light Theme</source>
+        <translation>Asentel acaɛlal</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="19"/>
+        <source>Dark Theme</source>
+        <translation>Asentel aberkan</translation>
+    </message>
+    <message>
+        <location filename="../resourceTS.cpp" line="20"/>
+        <source>System Theme</source>
+        <translation>Asentel n unagraw</translation>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="3"/>
+        <source>About Browser</source>
+        <translation>Γef yiminig</translation>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="4"/>
+        <source>This browser is made possible by the Chromium open source project and other open source software.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="6"/>
+        <source>Copyright</source>
+        <translation>Copyright</translation>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="7"/>
+        <source>UnionTech Software Technology Co., LTD. All Rights Reserved.</source>
+        <translation>UnionTech Software Technology Co., LTD. Akk izerfan ttwaḥerzen.</translation>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="9"/>
+        <source>Clear cookies and site data when you quit Browser</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="11"/>
+        <source>You don&apos;t have any extension, please install</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="13"/>
+        <source>You&apos;re viewing a secure Browser page</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../settingTS.cpp" line="14"/>
+        <source>Exit Browser anyway?</source>
+        <translation>Ffeɣ seg yiminig akken yebɣu yili?</translation>
+    </message>
+    <message>
+        <location filename="../SecureAccess.cpp" line="24"/>
+        <source>Malicious file</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../SecureAccess.cpp" line="25"/>
+        <source>Fake advertisement</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../SecureAccess.cpp" line="26"/>
+        <source>Information fraud</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../SecureAccess.cpp" line="27"/>
+        <source>Social engineering fraud</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../SecureAccess.cpp" line="29"/>
+        <source>This site %1 may harm your computer, please do not visit it to keep your computer safe!</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
