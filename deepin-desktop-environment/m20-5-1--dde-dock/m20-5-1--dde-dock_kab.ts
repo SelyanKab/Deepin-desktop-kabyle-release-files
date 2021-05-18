f84be422f76e4f7075f2564939b48e06d240dd05
SelyanKab <selyan.kab@protonmail.com>
2021-05-18 09:27:54 +0200

    dde-dock

---
+++ deepin-desktop-environment/m20-5-1--dde-dock/m20-5-1--dde-dock_kab.ts
@@ -0,0 +1,488 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="kab" version="2.1">
+<context>
+    <name>AbstractPluginsController</name>
+    <message>
+        <source>The plugin %1 is not compatible with the system.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>BluetoothApplet</name>
+    <message>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+    <message>
+        <source>Bluetooth settings</source>
+        <translation>Iɣewwaren n Bluetooth </translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothItem</name>
+    <message>
+        <source>Turn off</source>
+        <translation>Sens</translation>
+    </message>
+    <message>
+        <source>Turn on</source>
+        <translation>Sermed</translation>
+    </message>
+    <message>
+        <source>Bluetooth settings</source>
+        <translation>Iɣewwaren n Bluetooth </translation>
+    </message>
+    <message>
+        <source>%1 connected</source>
+        <translation>%1 yettwaqqen</translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation>Ala yetteqqen...</translation>
+    </message>
+    <message>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+    <message>
+        <source>Turned off</source>
+        <translation>Yensa</translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothPlugin</name>
+    <message>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>DBusAdaptors</name>
+    <message>
+        <source>Add keyboard layout</source>
+        <translation>Rnu taneɣruft n unasiw</translation>
+    </message>
+</context>
+<context>
+    <name>DatetimePlugin</name>
+    <message>
+        <source>Datetime</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>12-hour time</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>24-hour time</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Time settings</source>
+        <translation>Iɣewwaren n wakud</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceItem</name>
+    <message>
+        <source>Enable network</source>
+        <translation>Sermed azeḍḍa</translation>
+    </message>
+    <message>
+        <source>Disable network</source>
+        <translation>Sens azeḍḍa</translation>
+    </message>
+    <message>
+        <source>Network settings</source>
+        <translation>Iɣewwaren n uzeḍḍa</translation>
+    </message>
+</context>
+<context>
+    <name>DialogManager</name>
+    <message>
+        <source>Are you sure you want to empty %1 items?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Sefsex</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Kkes</translation>
+    </message>
+    <message>
+        <source>This action cannot be restored</source>
+        <translation>Tigawt-agi ulamek ara d-ttwarr</translation>
+    </message>
+</context>
+<context>
+    <name>DiskControlItem</name>
+    <message>
+        <source>Unknown device</source>
+        <translation>Ibenk arussin</translation>
+    </message>
+    <message>
+        <source>Unknown volume</source>
+        <translation>Ableɣ arussin</translation>
+    </message>
+</context>
+<context>
+    <name>DiskMountPlugin</name>
+    <message>
+        <source>Disk</source>
+        <translation>Aḍebsi</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Ldi</translation>
+    </message>
+    <message>
+        <source>Unmount all</source>
+        <translation>Kkes akk</translation>
+    </message>
+</context>
+<context>
+    <name>DockSettings</name>
+    <message>
+        <source>Fashion Mode</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Efficient Mode</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Afella</translation>
+    </message>
+    <message>
+        <source>Bottom</source>
+        <translation>Adda</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation>Azelmaḍ</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Ayfus</translation>
+    </message>
+    <message>
+        <source>Keep Shown</source>
+        <translation>Eǧǧ-it yettban</translation>
+    </message>
+    <message>
+        <source>Keep Hidden</source>
+        <translation>Eǧǧ-it yeffer</translation>
+    </message>
+    <message>
+        <source>Smart Hide</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Askar</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Adig</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Addad</translation>
+    </message>
+    <message>
+        <source>Plugins</source>
+        <translation>Iseɣzaf</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherItem</name>
+    <message>
+        <source>Launcher</source>
+        <translation>Amsekker</translation>
+    </message>
+</context>
+<context>
+    <name>MultitaskingPlugin</name>
+    <message>
+        <source>Multitasking View</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Undock</source>
+        <translation>Kkes asekkeṛ</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkItem</name>
+    <message>
+        <source>Disable wired connection</source>
+        <translation>Sens tuqqna n tnelli</translation>
+    </message>
+    <message>
+        <source>Enable wired connection</source>
+        <translation>Srmed tuqqna n tnelli</translation>
+    </message>
+    <message>
+        <source>Disable wireless connection</source>
+        <translation>Sens tuqqna war tinelli</translation>
+    </message>
+    <message>
+        <source>Enable wireless connection</source>
+        <translation>Sermed tuqqna war tinelli</translation>
+    </message>
+    <message>
+        <source>Disable network</source>
+        <translation>Sens azeḍḍa</translation>
+    </message>
+    <message>
+        <source>Enable network</source>
+        <translation>Sexsi azeḍḍa</translation>
+    </message>
+    <message>
+        <source>Network settings</source>
+        <translation>Iɣewwaren n uzeḍḍa</translation>
+    </message>
+    <message>
+        <source>Device disabled</source>
+        <translation>Ibenk yensa</translation>
+    </message>
+    <message>
+        <source>Wireless connection: %1</source>
+        <translation>Tuqqna war tinelli: %1</translation>
+    </message>
+    <message>
+        <source>Wired connection: %1</source>
+        <translation>Tuqqna s tnelli: %1</translation>
+    </message>
+    <message>
+        <source>Not connected</source>
+        <translation>Ur yeqqin ara</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>Ala yetteqqen</translation>
+    </message>
+    <message>
+        <source>Connected but no Internet access</source>
+        <translation>Yeqqen maca ulac adduf ɣer internet</translation>
+    </message>
+    <message>
+        <source>Network cable unplugged</source>
+        <translation>Agatu n uzeḍḍa yenser</translation>
+    </message>
+    <message>
+        <source>Wireless Network</source>
+        <translation>Azeḍḍa war tinelli</translation>
+    </message>
+    <message>
+        <source>Wired Network</source>
+        <translation>Azeḍḍa s tnelli</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkPlugin</name>
+    <message>
+        <source>Network</source>
+        <translation>Azeḍḍa</translation>
+    </message>
+    <message>
+        <source>Wired Network</source>
+        <translation>Azeḍḍa s tnelli</translation>
+    </message>
+    <message>
+        <source>Wired Network %1</source>
+        <translation>Azeḍḍa s tnelli %1</translation>
+    </message>
+</context>
+<context>
+    <name>OnboardPlugin</name>
+    <message>
+        <source>Onboard</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Iɣewwaren</translation>
+    </message>
+</context>
+<context>
+    <name>PowerPlugin</name>
+    <message>
+        <source>Power</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Power settings</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Capacity %1, %2 min remaining</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Capacity %1, %2 hr %3 min remaining</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Charging %1, %2 min until full</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Charging %1, %2 hr %3 min until full</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Charged</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Capacity %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Charging %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Capacity %1 ...</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Capacity %1, fully charged</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>ShowDesktopPlugin</name>
+    <message>
+        <source>Show Desktop</source>
+        <translation>Sken tanarit</translation>
+    </message>
+    <message>
+        <source>Undock</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>ShutdownPlugin</name>
+    <message>
+        <source>Power</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Shut down</source>
+        <translation>Sens</translation>
+    </message>
+    <message>
+        <source>Suspend</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Hibernate</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Lock</source>
+        <translation>Sekkeṛ</translation>
+    </message>
+    <message>
+        <source>Log out</source>
+        <translation>Ffeɣ</translation>
+    </message>
+    <message>
+        <source>Switch account</source>
+        <translation>Snifel amiḍan</translation>
+    </message>
+    <message>
+        <source>Power settings</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>Ales asekker</translation>
+    </message>
+</context>
+<context>
+    <name>SoundApplet</name>
+    <message>
+        <source>Device</source>
+        <translation>Ibenk</translation>
+    </message>
+    <message>
+        <source>Application</source>
+        <translation>Asnas</translation>
+    </message>
+</context>
+<context>
+    <name>SoundItem</name>
+    <message>
+        <source>Unmute</source>
+        <translation>Serreḥ i yimesli</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Gzem imesli</translation>
+    </message>
+    <message>
+        <source>Sound settings</source>
+        <translation>Iɣewwaren n yimesli</translation>
+    </message>
+    <message>
+        <source>Volume %1</source>
+        <translation>Ableɣ %1</translation>
+    </message>
+</context>
+<context>
+    <name>SoundPlugin</name>
+    <message>
+        <source>Sound</source>
+        <translation>Imesli</translation>
+    </message>
+</context>
+<context>
+    <name>TrashPlugin</name>
+    <message>
+        <source>Trash</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Trash - %1 file</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Trash - %1 files</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>TrashWidget</name>
+    <message>
+        <source>Open</source>
+        <translation>Ldi</translation>
+    </message>
+    <message>
+        <source>Empty</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>TrayPlugin</name>
+    <message>
+        <source>System Tray</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>WirelessList</name>
+    <message>
+        <source>Wireless Network</source>
+        <translation>Azeḍḍa war tinelli</translation>
+    </message>
+    <message>
+        <source>Wireless Network %1</source>
+        <translation>Azeḍḍa s tnelli %1</translation>
+    </message>
+</context>
+</TS>
